c4e7ab67a011fbeb2d51ffae5a4aa0de1324c5b2
ab00zar <rajabi.aboozar@gmail.com>
2025-03-09 17:20:40 +0100

    QueryParser error handling & specs

--- app/services/carts/query_parser.rb
+++ app/services/carts/query_parser.rb
@@ -1,10 +1,29 @@
 module Carts
   class QueryParser
-    def self.parse(query)
-      query
-        .split(',')
+    ITEM_PATTERN = /^\d+ [A-Z0-9]+$/
+
+    def initialize(query)
+      @query = query
+    end
+
+    def parse
+      validate_query
+      items = @query.split(',').map(&:strip)
+      items.each { |item| validate_item(item) }
+
+      items
         .map { |pair| pair.split(' ') }
         .map { |quantity, code| [quantity.to_i, code] }
+    end
+
+    private
+
+    def validate_query
+      raise ArgumentError, "Query must be a string" unless @query.is_a?(String)
+    end
+
+    def validate_item(item)
+      raise ArgumentError, "Invalid item format: '#{item}'" unless item.match?(ITEM_PATTERN)
     end
   end
 end
---
+++ spec/services/carts/query_parser_spec.rb
@@ -0,0 +1,37 @@
+RSpec.describe Carts::QueryParser do
+  describe '#parse' do
+    let(:subject) { described_class.new(query).parse }
+
+    context 'with queries in a good format' do
+      let(:query) { '2 ITEM,  3 ANOTHER ' }
+
+      it 'handles the whitespaces' do
+        expect(subject).to eq([[2, 'ITEM'], [3, 'ANOTHER']])
+      end
+
+      it 'converts the quantity to integer' do
+        expect(subject.first.first.is_a?(Integer)).to be_truthy
+      end
+    end
+
+    context 'with malformed queries' do
+      context 'when receiving a query rather than a string' do
+        let(:query) { ['2 ITEM'] }
+        let(:parser) { described_class.new(query).parse }
+
+        it 'raise an error' do
+          expect { parser }.to raise_error(ArgumentError, 'Query must be a string')
+        end
+      end
+
+      context 'when the desired format is not followed' do
+        let(:query) { 'A UUY 2' }
+        let(:parser) { described_class.new(query).parse }
+
+        it 'raise an error' do
+          expect { parser }.to raise_error(ArgumentError, "Invalid item format: 'A UUY 2'")
+        end
+      end
+    end
+  end
+end
