eca8004ae3a8dc60b1c78ee88cbbac6e93db0ee4
ab00zar <rajabi.aboozar@gmail.com>
2025-03-10 01:58:32 +0100

    product model specs added

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,24 @@
-require 'rails_helper'
-
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it 'has many discounts' do
+      product = Product.new(code: 'P001', name: 'Test Product', price: 10.0)
+      discount1 = Discount.new(min_quantity: 1, percentage: 10.0)
+      discount2 = Discount.new(min_quantity: 5, percentage: 20.0)
+
+      product.discounts << discount1
+      product.discounts << discount2
+
+      expect(product.discounts.size).to eq(2)
+      expect(product.discounts).to include(discount1, discount2)
+    end
+
+    describe 'validations' do
+      context 'with valid attributes' do
+        it 'is valid' do
+          product = Product.new(code: 'P001', name: 'Test Product', price: 10.0)
+          expect(product).to be_valid
+        end
+      end
+    end
+  end
 end
