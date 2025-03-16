6e913ac5d658fdec7c8b12ff464c1b7677e76a83
ab00zar <rajabi.aboozar@gmail.com>
2025-03-17 00:23:58 +0100

    product code format validation updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ApplicationRecord
   has_many :discounts
 
-  validates :code, presence: :true, uniqueness: true
-  validates :name, presence: :true
-  validates :price, presence: :true, numericality: { greater_than: 0 }
+  VALID_CODE_FORMAT = /\A[A-Z0-9]+\z/
+
+  validates :code, presence: true,
+            uniqueness: true,
+            format: { with: VALID_CODE_FORMAT, message: "Must contain only uppercase letters and numbers!" }
+  validates :name, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
